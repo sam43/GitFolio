fc7f98e9401e5be4b247e614923e0100683397cc
Sadat Sayem <scode43@gmail.com>
2025-07-23 11:17:30 +0900

    Refactor: Rebased with master branch and tested if the features are working.

--- app/src/main/java/io/sam43/gitfolio/presentation/screens/SettingsScreen.kt
+++ app/src/main/java/io/sam43/gitfolio/presentation/screens/SettingsScreen.kt
@@ -74,7 +74,7 @@ fun SettingsScreen(
         AlertDialog(
             onDismissRequest = { showAboutDialog = false },
             title = { Text(text = "What\'s init?", fontWeight = FontWeight.Bold, fontFamily = FontFamily.Monospace) },
-            text = { Text(text = "\n1.GitHub Explorer: Easily search and view GitHub user profiles and their repositories.\n2.Modern Android: Built with the latest technologies including Jetpack Compose (UI), Kotlin Coroutines, and MVVM architecture.\n3.Smooth Experience: Features Material 3 design and Shared Element Transitions for seamless navigation.\n4.Core Tech: Utilizes Retrofit for API calls, Coil for image loading, and Hilt for dependency management.\n5.Developer: Created by Sadat Sayem. \nContact: scode43@gmail.com", fontFamily = FontFamily.Monospace) },
+            text = { Text(text = "\n1.GitHub Explorer: Easily search and view GitHub user profiles and their repositories.\n2.Modern Android: Built with the latest technologies including Jetpack Compose (UI), Kotlin Coroutines, and MVI architecture.\n3.Smooth Experience: Features Material 3 design and Shared Element Transitions for seamless navigation.\n4.Core Tech: Utilizes Retrofit for API calls, Coil for image loading, and Hilt for dependency management.\n5.Developer: Created by Sadat Sayem. \nContact: scode43@gmail.com", fontFamily = FontFamily.Monospace) },
             confirmButton = {
                 Button(onClick = { showAboutDialog = false }) {
                     Text(text = "Thanks!", fontFamily = FontFamily.Monospace)
